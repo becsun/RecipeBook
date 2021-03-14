ec42df2cd13f90895a64de2de68430a2ff3a1181
bec <becsun@icloud.com>
2021-03-14 15:04:01 +0000

    instructions added to the mothod for picked apple and ingredients needed

--- src/com/recipebookproject/applepie/Apples.java
+++ src/com/recipebookproject/applepie/Apples.java
@@ -11,12 +11,21 @@ public class Apples {
     }
     TypeOfApples apple = null;
 
+    String[] classic = new String[]{};
+    String[] dutch = new String[]{};
+
     public TypeOfApples getApple() {
         return apple;
     }
 
     public void setApple(TypeOfApples apple) {
         this.apple = apple;
+    }
+    public void pickedApple(){
+        System.out.println("i want "+apple+ " apples in my classic apple pie");
+    }
+    public void ingredientsNeeded(){
+        System.out.println("add"+ dutch.length);
     }
 
 }
