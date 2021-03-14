caa23dae4050850b84f3368be3d02817bb4ceddd
bec <becsun@icloud.com>
2021-03-14 12:29:05 +0000

    pie function added for the different pie ingredients

--- src/com/recipebookproject/applepie/Pie.java
+++ src/com/recipebookproject/applepie/Pie.java
@@ -1,4 +1,11 @@
 package com.recipebookproject.applepie;
 
 public class Pie {
+    public Classic(){
+        this.ingredients = new String[]{"flour", "sugar", "water"};
+    }
+
+    public Dutch( {
+        this.ingredients = new String[]{"pastry", "sugar", "water"};
+    })
 }
