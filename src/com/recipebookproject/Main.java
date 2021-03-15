767e4ccd76163036d88338f0a07806eaeb1833cf
bec <becsun@icloud.com>
2021-03-15 22:09:42 +0000

    change the first question

--- src/com/recipebookproject/Main.java
+++ src/com/recipebookproject/Main.java
@@ -6,7 +6,7 @@ import com.recipebookproject.applepie.Pie;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("What kind of pie would you like to make?\n1.grannysmith\n2. jazz \n3.reddelicious\n4.fuji ");
+        System.out.println("Which kind of apple would you like to put in you apple pie?\n1.grannysmith\n2. jazz \n3.reddelicious\n4.fuji ");
 
 //	    var classicApplePie = new Pie();
 //        classicApplePie.setApple(Apples.TypeOfApples.grannysmith);
