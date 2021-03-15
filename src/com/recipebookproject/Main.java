93e947c01f8b1f7cba41c3039a61e5e5de0ce35c
bec <becsun@icloud.com>
2021-03-15 22:08:04 +0000

    changing the app so that it will take the input from the user instead of hard coding

--- src/com/recipebookproject/Main.java
+++ src/com/recipebookproject/Main.java
@@ -1,13 +1,16 @@
 package com.recipebookproject;
 
 import com.recipebookproject.applepie.Apples;
+import com.recipebookproject.applepie.Pie;
 
 public class Main {
 
     public static void main(String[] args) {
-	    var classicApplePie = new Apples();
-        classicApplePie.setApple(Apples.TypeOfApples.grannysmith);
-        classicApplePie.pickedApple();
-        classicApplePie.ingredientsNeeded();
+        System.out.println("What kind of pie would you like to make?\n1.grannysmith\n2. jazz \n3.reddelicious\n4.fuji ");
+
+//	    var classicApplePie = new Pie();
+//        classicApplePie.setApple(Apples.TypeOfApples.grannysmith);
+//        classicApplePie.pickedApple();
+//        classicApplePie.ingredientsNeeded();
     }
 }
