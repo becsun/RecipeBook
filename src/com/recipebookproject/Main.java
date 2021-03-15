72024afdd198061b8250742eaf6a6969cb413f59
bec <becsun@icloud.com>
2021-03-15 22:18:29 +0000

    Scanner added to get user input

--- src/com/recipebookproject/Main.java
+++ src/com/recipebookproject/Main.java
@@ -3,14 +3,18 @@ package com.recipebookproject;
 import com.recipebookproject.applepie.Apples;
 import com.recipebookproject.applepie.Pie;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Which kind of apple would you like to put in you apple pie?\n1.grannysmith\n2. jazz \n3.reddelicious\n4.fuji ");
-
+        int appleChoice = scanner.nextInt();
 //	    var classicApplePie = new Pie();
 //        classicApplePie.setApple(Apples.TypeOfApples.grannysmith);
 //        classicApplePie.pickedApple();
 //        classicApplePie.ingredientsNeeded();
+        
     }
 }
