56ab1c34474dca9e85555cac0335c30ea374793d
bec <becsun@icloud.com>
2021-03-17 11:41:47 +0000

    while loop and conditional put in to decide which apple is picked. Need to refactor the code

--- src/com/recipebookproject/Main.java
+++ src/com/recipebookproject/Main.java
@@ -2,19 +2,41 @@ package com.recipebookproject;
 
 import com.recipebookproject.applepie.Apples;
 import com.recipebookproject.applepie.Pie;
-
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Which kind of apple would you like to put in you apple pie?\n1.grannysmith\n2. jazz \n3.reddelicious\n4.fuji ");
+        System.out.println("Which kind of apple would you like to put in you apple pie?\ngrannysmith\njazz \nreddelicious\nfuji ");
         int appleChoice = scanner.nextInt();
-//	    var classicApplePie = new Pie();
-//        classicApplePie.setApple(Apples.TypeOfApples.grannysmith);
-//        classicApplePie.pickedApple();
-//        classicApplePie.ingredientsNeeded();
-        
+        System.out.println(appleChoice);
+
+
+
+        var classicApplePie = new Apples();
+        while (appleChoice >4 || appleChoice<0){
+            System.out.println("Must be between 1 and 4");
+            appleChoice = scanner.nextInt();
+        }
+        if( appleChoice == 1) {
+            classicApplePie.setApple(Apples.TypeOfApples.grannysmith);
+        } else if ( appleChoice ==2 ) {
+            classicApplePie.setApple(Apples.TypeOfApples.jazz);
+        } else if ( appleChoice ==3 ) {
+            classicApplePie.setApple(Apples.TypeOfApples.reddelicious);
+        }else if ( appleChoice ==4 ) {
+            classicApplePie.setApple(Apples.TypeOfApples.fuji);
+
+            }
+        classicApplePie.pickedApple();
+
+
+
+
+
+
+////        classicApplePie.ingredientsNeeded();
+
     }
 }
